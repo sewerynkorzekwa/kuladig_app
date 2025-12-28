b1631e9e877f1a6868f59899d1e62c2be298756b
Seweryn Korzekwa <seweryn.korzekwa@icloud.com>
2025-12-28 02:19:01 +0100

    Add ExperimentalMaterial3Api annotation in MainActivity for compatibility with Material3 components

--- app/src/main/java/com/example/kuladig_app/MainActivity.kt
+++ app/src/main/java/com/example/kuladig_app/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -39,6 +40,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @PreviewScreenSizes
 @Composable
 fun Kuladig_appApp() {
