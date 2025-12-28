90c4c7c826a986322b44c46234bb5c2cd59188b6
Seweryn Korzekwa <seweryn.korzekwa@icloud.com>
2025-12-28 02:16:02 +0100

    Replace SmallTopAppBar with TopAppBar in MainActivity for enhanced UI consistency

--- app/src/main/java/com/example/kuladig_app/MainActivity.kt
+++ app/src/main/java/com/example/kuladig_app/MainActivity.kt
@@ -12,8 +12,8 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -64,7 +64,7 @@ fun Kuladig_appApp() {
         Scaffold(
             modifier = Modifier.fillMaxSize(),
             topBar = {
-                SmallTopAppBar(
+                TopAppBar(
                     title = { Text(currentDestination.label) }
                 )
             }
